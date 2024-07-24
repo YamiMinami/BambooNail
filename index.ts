ca9d719901f7def873a50a54079c6e918003eaab
s117509 <s117509@ap.be>
2024-07-24 20:57:21 +0000

    updated

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 import express from "express";
 const app = express();
+
 app.set("view engine", "ejs");
 app.set("port", 3000);
-app.use(express.static("public"));
 
-app.get("/", (req, res) => {
+app.get("/",(req, res)=>{
     res.render("index");
-});
+})
 
 app.listen(app.get("port"), () =>
 console.log("[server] http://localhost:" + app.get("port"))
